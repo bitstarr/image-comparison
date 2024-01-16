25605c0dc05f7acd28876923181ea2b9d375c3e4
Sebastian Laube <sebastian.laube@zebra.de>
2024-01-16 15:24:08 +0100

    initial commit

---
+++ image-comparison.js
@@ -0,0 +1,53 @@
+class ImageComparison extends HTMLElement
+{
+    static tagName = 'image-comparison';
+
+    constructor()
+    {
+        super();
+        // element created
+    }
+
+    connectedCallback()
+    {
+        // browser calls this method when the element is added to the document
+        // (can be called many times if an element is repeatedly added/removed)
+
+        if ( !this.style.getPropertyValue( '--value' ) )
+        {
+            this.setProp( 'value', '50%' );
+        }
+
+        if ( this.hasAttribute( 'aspect-ratio' ) )
+        {
+            this.setProp( 'ar', this.getAttribute( 'aspect-ratio' ) );
+        }
+
+        this.slidable();
+    }
+
+    slidable()
+    {
+        this.slider = document.createElement( 'input' );
+        this.slider.type = 'range';
+        this.slider.min = 0;
+        this.slider.max = 100;
+        this.slider.value = 50;
+        this.slider.classList.add( 'range' );
+        this.append( this.slider );
+
+        this.slider.addEventListener( 'input', (e) =>
+        {
+            this.setProp( 'value', `${e.target.value}%` );
+        });
+    }
+
+    setProp( name, value )
+    {
+        this.style.setProperty( `--${name}`, value );
+    }
+}
+
+if ( 'customElements' in window ) {
+	customElements.define( ImageComparison.tagName, ImageComparison );
+}
---
+++ readme.md
@@ -0,0 +1,25 @@
+# `<image-comparison>`
+
+A web component to create an image comparison element. Based off of [this Codepen](https://codepen.io/stoumann/pen/VwKgJNv) from [Mads Stoumann](https://codepen.io/stoumann).
+
+[Check out the Demo](https://bitstarr.github.io/image-comparison/demo.html)
+
+## Usage
+
+```html
+<script type="module" src="image-comparison.js"></script>
+<link rel="stylesheet" href="image-comparison.css">
+
+<image-comparison class="imgcompare">
+    <img class="left" src="https://picsum.photos/960/540?random=1" alt="A random image from Unsplash" />
+    <img class="right" src="https://picsum.photos/960/540?random=2" alt="Another random image from Unsplash" />
+</image-comparison>
+```
+
+### Change aspect ratio
+
+```html
+<image-comparison aspect-ratio="4/3">
+    …
+</image-comparison>
+```
